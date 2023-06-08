fe94021ee3429f7b21a164333eee8fc1b787a792
Riddhi <riddhi@firsteconomy.com>
2023-06-08 15:18:32 +0530

    merge dev branch

--- database/migrations/2023_06_08_152843_create_media_table.php
+++ database/migrations/2023_06_08_152843_create_media_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsTable extends Migration
+class CreateMediaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,26 +13,20 @@ class CreateBlogsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('media', function (Blueprint $table) {
             $table->id();
-            $table->string('meta_title')->nullable();
-            $table->string('meta_keyword')->nullable();
-            $table->text('meta_description')->nullable();
-            $table->string('canonical')->nullable();
             $table->string('title')->nullable();
             $table->string('tag')->nullable();
             $table->string('slug')->nullable();
             $table->text('thumbnail_image')->nullable();
-            $table->text('image')->nullable();
-            $table->text('desk_banner')->nullable();
-            $table->text('mob_banner')->nullable();
             $table->string('short_desc')->nullable();
-            $table->text('description')->nullable();
             $table->date('date')->nullable();
-            $table->enum('type', ['Blogs', 'Expert Speaks','Fitness Trends & Updates'])->default('Blogs');
+            $table->string('news_link')->nullable();
+            $table->string('news_url')->nullable();
+            $table->enum('type', ['Video', 'News','image'])->nullable();
             $table->tinyInteger('status')->default(0);
             $table->timestamps();            
-            $table->softDeletes();  
+            $table->softDeletes();
         });
     }
 
@@ -43,6 +37,6 @@ class CreateBlogsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('media');
     }
 }
